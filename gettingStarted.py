3d70983b1d090c85b2a7ec840241924fcc235ce7
ekh8907-dev <ekh8907@nyu.edu>
2026-09-11 23:37:18 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -29,20 +29,19 @@ def welcome_assignment_answers(question):
         answer = f"ERROR: unrecognized answer -> {question!r}"
     return(answer)
 # Complete all the questions.
-    
-    if __name__ == "__main__":
+if __name__ == "__main__":
     #use this space to debug and verify that the program works
     #writing code to check all questions instead of one
-        debug_questions = [
-            "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?",
-            "Are encoding and encryption the same? - Yes/No",
-            "Is it possible to decrypt a message without a key? - Yes/No",
-            "Is it possible to decode a message without a key? - Yes/No",
-            "Is a hashed message supposed to be un-hashed? - Yes/No",
-            "What is the SHA256 hashing value of your NYU email and use the answer in your code - ",
-            "Is MD5 a secured hashing algorithm? - Yes/No",
-            "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number",
-            "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number",
-        ]
-        for q in debug_questions:
-            print(f"{q}\n -> {welcome_assignment_answers(q)}\n")
+    debug_questions = [
+        "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?",
+        "Are encoding and encryption the same? - Yes/No",
+        "Is it possible to decrypt a message without a key? - Yes/No",
+        "Is it possible to decode a message without a key? - Yes/No",
+        "Is a hashed message supposed to be un-hashed? - Yes/No",
+        "What is the SHA256 hashing value of your NYU email and use the answer in your code - ",
+        "Is MD5 a secured hashing algorithm? - Yes/No",
+        "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number",
+        "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number",
+    ]
+    for q in debug_questions:
+        print(f"{q}\n -> {welcome_assignment_answers(q)}\n")
